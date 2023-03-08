ea889cace3849a69956f2727c544eaf006202c29
stovecbr <pc-018@st-ove.co.jp>
2023-03-08 10:09:47 +0900

    テキスト表記変更

--- components/maintitle.js
+++ components/maintitle.js
@@ -1,7 +1,12 @@
+import styles from '@/styles/Home.module.css'
+
 export function MainTitle(props){
   return(
-    <div >
-      <h1>{props.maintitle} PAGE</h1> 
-    </div>
+    // <div >
+    //   <h1>{props.maintitle} PAGE</h1> 
+    // </div>
+    <div className={styles.title}>
+      <h1>{props.maintitle} page</h1> 
+    </div>         
   )
 }
