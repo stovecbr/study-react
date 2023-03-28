5574f54288360b4d113d06880943616edc6eca7c
stovecbr <pc-018@st-ove.co.jp>
2023-03-28 11:45:50 +0900

    ⚡️ useCallback,useEffectの第二引数に関して

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,27 +2,21 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Main } from "@/src/components/main";
 import { HeaderLink } from "@/src/components/HeaderLink";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
-
-// const handleClick = (e, foo) => {
-//   console.log(e.target.href);
-//   e.preventDefault();
-//   alert(foo);
-// };
 
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightpink";
-
     return () => {
       document.body.style.backgroundColor = "";
     };
