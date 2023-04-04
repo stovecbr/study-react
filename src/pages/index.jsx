eda67f60ef60141356b7daa44d84fab2bdc83bef
stovecbr <pc-018@st-ove.co.jp>
2023-04-04 13:29:12 +0900

    ♻️ 状態を変更するときにprevをつけて名前をわかりやすくした。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,13 +13,13 @@ export default function Home() {
 
   const handleClick = useCallback(() => {
     if (count < 10) {
-      setCount((count) => count + 1);
+      setCount((prevCount) => prevCount + 1);
     }
   }, [count]);
 
   const handleDisplay = useCallback(() => {
-    setIsShow((isShow) => !isShow);
-  },[]);
+    setIsShow((prevIsShow) => !prevIsShow);
+  }, []);
   
   const handleChange = useCallback((e) => {
     if (e.target.value.length > 5) {
